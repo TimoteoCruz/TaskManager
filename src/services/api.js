5ce1051b9ccccba9c228cd416f244b9dffafc28c
Timoteo <titohdez31@gmail.com>
2025-03-08 13:59:03 -0600

    Definitivo

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE_URL="http://localhost:3000/api"
+const API_BASE_URL="https://taskmanager-backend-8fhk.onrender.com/api"
 
 const api = axios.create({
     baseURL: API_BASE_URL,
